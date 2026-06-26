9fe70668ce044dabaf26825487c47f42d20c4925
Pablo-numsei <augustopablo620@gmail.com>
2026-06-26 11:19:56 +0000

    mudanças oara aplicação no vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Configuração otimizada para deploy na raiz do Vercel
 export default defineConfig({
   plugins: [react()],
-  base: '/router-dom-viacep/',
+  base: '/', 
 })
