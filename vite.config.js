1d840471d727041f4ff8f5a014e927321457b656
Pablo-numsei <augustopablo620@gmail.com>
2026-06-26 11:28:34 +0000

    fix: corrige base url do vite para raiz

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Configuração otimizada para deploy na raiz do Vercel
 export default defineConfig({
   plugins: [react()],
   base: '/', 
